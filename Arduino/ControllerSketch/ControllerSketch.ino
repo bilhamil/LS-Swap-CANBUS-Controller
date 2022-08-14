ea30cf16fbc0b6b0ecc796b928ec3dadfda83032
Bill Hamilton <bilhamil@nmsu.edu>
2022-08-13 21:10:30 -0600

    Catching up

--- Arduino/ControllerSketch/ControllerSketch.ino
+++ Arduino/ControllerSketch/ControllerSketch.ino
@@ -7,13 +7,17 @@
 struct can_frame canMsg;
 struct can_frame AC_ON_MSG;
 struct can_frame AC_OFF_MSG;
-struct can_frame AC_ON_MSG;
-struct can_frame AC_OFF_MSG;
+
+HardwareSerial SerialHWD(USART1);   // PA3  (RX)  PA2  (TX)
+
 MCP2515 mcp2515(PB13);
 
 int pinVal = 0;
+int messageCount = 0;
 bool pin = true;
 bool onOff = true;
+
+long timeSinceLastRead = millis();
 
 void setup() {
   //AC Serial ON 1F1 AE 16 00 00 08 00 00 7A 
@@ -31,16 +35,16 @@ void setup() {
 
   //AC Serial OFF 1F1 AE 0E 00 00 08 00 00 7A 
   //src: https://pcmhacking.net/forums/viewtopic.php?f=38&t=6962&start=10
-  AC_ON_MSG.can_id  = 0x1F1;
-  AC_ON_MSG.can_dlc = 8;
-  AC_ON_MSG.data[0] = 0xAE;
-  AC_ON_MSG.data[1] = 0x0E;
-  AC_ON_MSG.data[2] = 0x00;
-  AC_ON_MSG.data[3] = 0x00;
-  AC_ON_MSG.data[4] = 0x08;
-  AC_ON_MSG.data[5] = 0x00;
-  AC_ON_MSG.data[6] = 0x00;
-  AC_ON_MSG.data[7] = 0x7A;
+  AC_OFF_MSG.can_id  = 0x1F1;
+  AC_OFF_MSG.can_dlc = 8;
+  AC_OFF_MSG.data[0] = 0xAE;
+  AC_OFF_MSG.data[1] = 0x0E;
+  AC_OFF_MSG.data[2] = 0x00;
+  AC_OFF_MSG.data[3] = 0x00;
+  AC_OFF_MSG.data[4] = 0x08;
+  AC_OFF_MSG.data[5] = 0x00;
+  AC_OFF_MSG.data[6] = 0x00;
+  AC_OFF_MSG.data[7] = 0x7A;
   
   // initialize digital pin PB2 as an output.
   pinMode(PC13, OUTPUT); // LED connect to pin PB2
@@ -48,7 +52,7 @@ void setup() {
   pinMode(PB0, OUTPUT);
 
   //  setup serial
-  //Serial.begin(4000);
+  SerialHWD.begin(115200);
   pinMode(PB12, INPUT_PULLUP);
   SPI.setMOSI(PB5);
   SPI.setMISO(PB4);
@@ -68,25 +72,25 @@ void setup() {
   mcp2515.setNormalMode();
 }
 
+void printCan(can_frame &canMSG)
+{
+  SerialHWD.printf("ID: %X", canMsg.can_id);
+  SerialHWD.print(" Data: ");
+    
+  for (int i = 0; i < canMsg.can_dlc; i++)  { // print the data
+    SerialHWD.printf("%02X ", canMsg.data[i]);
+  }
+}
+
 void loop() {
-  /*pinVal = analogRead(PA1);
-  if(pinVal > 1000)
+  if(millis() - timeSinceLastRead > 250)
   {
-    digitalWrite(PC13, HIGH);
+    pinVal = analogRead(PA1);
+    //SerialHWD.printf("Pin Val: %d\r\n", pinVal);
+    timeSinceLastRead = millis();
   }
-  else
-  {
-    digitalWrite(PC13, LOW);
-  }
-
-  pin = !pin;
   
-  digitalWrite(PB0, pin?HIGH:LOW);
-  
-  
-  delay(2000);*/
-
-  if (mcp2515.readMessage(MCP2515::RXB0, &canMsg) == MCP2515::ERROR_OK) {
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
     
     //digitalWrite(PC13, LOW);
     if(canMsg.can_id == 0x1F5 && canMsg.data[3] == 0x01)
@@ -98,7 +102,10 @@ void loop() {
         digitalWrite(PC13, LOW);
     } 
       onOff = !onOff;
-    
+    SerialHWD.printf("Message: %i ", messageCount);
+    printCan(canMsg);
+    SerialHWD.println();
+    messageCount++;
   }
 
   //delay(50);
